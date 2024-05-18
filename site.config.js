00c9bf5f49c70889f1a5cf403691fda3ef00450b
jiwoo <89989211+jwo1024@users.noreply.github.com>
2024-05-19 03:32:44 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Jiwoo's blog",
+    description: "welcome to Jiwoo's blog!",
   },
 
   // CONFIG configration (required)
